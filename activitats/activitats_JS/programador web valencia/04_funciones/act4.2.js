de567978c63656bc2b6bb8111a0db1a803a8cbb9
PauP08 <pmpellisa@outlook.es>
2025-03-17 17:58:39 +0000

    Deploying to gh-pages from @ PauP08/programacio_web@7907f667fc336a64a4140ac29e58f6d2e65ae99f 🚀

---
+++ activitats/activitats_JS/programador web valencia/04_funciones/act4.2.js
@@ -0,0 +1,8 @@
+function alturaFormateada(){
+    var centimetros = prompt("Cuan medeixes? en CM");
+    var metres = centimetros / 100;
+    console.log(`medeixes ${metres} m`)
+    alert(`medeixes ${metres} m`)
+};
+
+alturaFormateada()
---
+++ activitats/activitats_JS/provesJS/prova3.js
@@ -0,0 +1,4 @@
+var a = 3;
+b = 5;
+c = a + b; // 8
+console.log(c);
