27e4be200514c86cfca215fe7330039abe903afc
PauP08 <pmpellisa@outlook.es>
2025-03-24 18:51:43 +0100

    /**//**//**//**//**//**//**/

---
+++ activitats/activitats_JS/provesJS/prova6.js
@@ -0,0 +1,17 @@
+const cadenaNormal = 'El 90% de nuestras decisiones las toma nuestro subconsciente';
+
+console.log(cadenaNormal)
+
+// Convierte en mayusculas
+const cadenaMayusculas = cadenaNormal.toUpperCase();
+
+console.log(cadenaMayusculas)
+
+// Convierte en minúsculas
+const cadenaMinusculas = cadenaNormal.toLowerCase();
+
+console.log(cadenaMinusculas)
+
+// Número de carácteres
+const longitud = cadenaNormal.length;
+console.log(longitud)
