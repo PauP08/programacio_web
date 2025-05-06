46d73df46ea44987c860eaa1f670a45905b67d44
PauP08 <pmpellisa@outlook.es>
2025-05-06 13:36:22 +0000

    Deploying to gh-pages from @ PauP08/programacio_web@3a992fef708c8f99f25bf6413e052c6cf3944018 🚀

--- activitats/projecte_calculadora/script.js
+++ activitats/projecte_calculadora/script.js
@@ -1,3 +1,23 @@
-const pantalla = document.getElementById('pantalla')
-const btnNumero = document.getElementById('btn7')
+const pantalla = document.getElementById('pantalla');
+const btnNumero = document.querySelectorAll('.numero');
 
+
+btnNumero.forEach(boto => {
+    boto.addEventListener('click', function(){
+        if(pantalla.textContent === '0'){
+            pantalla.textContent = boto.textContent
+        }else{
+            pantalla.textContent += boto.textContent
+        }
+    } )
+});
+
+
+
+// document.getElementById("btn7").addEventListener('click', function (){
+//     if(pantalla.textContent === "0"){
+//         document.getElementById('pantalla').innerHTML = btn7.textContent
+//     }else{
+//         pantalla.textContent += btn7.textContent
+//     }
+// } )
